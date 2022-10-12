da16f7c0c2d50d31766ede0fee4d415fbcf59e2c
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2022-10-12 14:46:19 +0545

    addition of the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
-  return (
-    <div className='App'>
-      welcome to hotel reservation for my junior developer
-
-    </div>
-    )
+ return (
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/hotels' element={<List/>} />
+    <Route path='/hotels/:id' element={<Hotel/>} />
+  </Routes>
+  </BrowserRouter>
+ )
 }
 
 export default App;
